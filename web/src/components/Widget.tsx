54bf8b77dae2fdc4bf87745ac8f34140fa9b2b27
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-04 16:31:28 -0300

    construindo o ícone com modo aberto, fechado e exibindo

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -3,8 +3,12 @@ import { ChatTeardropDots } from "phosphor-react";
 export function Widget(){
     return (
         <div className="absolute bottom-5 right-5">
-            <button className="bg-brand-500 rounded-full px-3 h-12 text-white">
-                <ChatTeardropDots className="w-6 h-6"/>
+            <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+                <ChatTeardropDots className="w-6 h-6"/>                
+                <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+                    <span className="pl-2"></span>
+                    Feedback
+                </span>
             </button>
         </div>
     )
