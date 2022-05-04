1f185dad7dd0bfb1e9d9ef1fcb08e46d7cdf096a
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-04 17:47:51 -0300

    popover da headless

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,24 +1,18 @@
 import { ChatTeardropDots } from "phosphor-react";
-import { useState } from "react";
+import { Popover } from "@headlessui/react"
 
 export function Widget(){
 
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-    function toggleWidgetVisibility(){
-        setIsWidgetOpen(!isWidgetOpen)
-    }
-
     return (
-        <div className="absolute bottom-5 right-5">
-            { isWidgetOpen && <p>Hello World.</p> }
-            <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">                
+        <Popover className="absolute bottom-5 right-5">
+            <Popover.Panel>Hello World.</Popover.Panel>
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">                
                 <ChatTeardropDots className="w-6 h-6"/>                
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
                     <span className="pl-2"></span>
                     Feedback
                 </span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
 }
