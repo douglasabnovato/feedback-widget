dda34970c07ec4cbf1f2916cbbfcf2eb647c13c9
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-04 16:48:03 -0300

    toggle do botão

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,9 +1,19 @@
 import { ChatTeardropDots } from "phosphor-react";
+import { useState } from "react";
 
 export function Widget(){
+
+    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+    function toggleWidgetVisibility(){
+        setIsWidgetOpen(!isWidgetOpen)
+    }
+
+
     return (
         <div className="absolute bottom-5 right-5">
-            <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+            { isWidgetOpen && <p>Hello World.</p> }
+            <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">                
                 <ChatTeardropDots className="w-6 h-6"/>                
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
                     <span className="pl-2"></span>
