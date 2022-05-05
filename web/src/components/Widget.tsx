a7e9edcd5bcc0b3f8353faec15502777f67c065a
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-05 16:43:31 -0300

    estilização do header e do footer

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -6,8 +6,10 @@ import { WidgetForm } from "./WidgetForm";
 export function Widget(){
 
     return (
-        <Popover className="absolute bottom-5 right-5">
-            <Popover.Panel className="text-white"><WidgetForm/></Popover.Panel>
+        <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
+            <Popover.Panel className="text-white">
+                <WidgetForm/>
+            </Popover.Panel>
             <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">                
                 <ChatTeardropDots className="w-6 h-6"/>                
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,6 +1,19 @@
 export function WidgetForm(){
 
     return (
-        <div>Hello Word, dev!</div>
+        <div className="bg-zinc-900 p-4 relative rounded-2x1 mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem) md:w-auto]">
+            
+            <header>
+                <h2>Header</h2>
+            </header>
+
+            <p>Hello Word, dev!</p>
+            
+            <footer>
+                <h2>Footer</h2>
+            </footer>
+            
+        </div>
     )
+
 }
