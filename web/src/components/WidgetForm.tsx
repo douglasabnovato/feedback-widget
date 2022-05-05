f5ee254592b6ecbbf5a58163c9e1c346281fe3fd
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-05 18:10:42 -0300

    construindo os conteúdos do widget

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,5 +1,33 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImageUrl from "../assets/bug.svg";
+import ideaImageUrl from "../assets/idea.svg";
+import thoughtImageUrl from "../assets/thought.svg";
+
+const feedbackTypes = {
+    BUG: {
+        title: "Problema",
+        image: {
+            source: bugImageUrl,
+            alt: "Imagem de um inseto"
+        },
+    },
+    IDEA: {
+        title: "Ideia",
+        image: {
+            source: ideaImageUrl,
+            alt: "Imagem de uma lâmpada"
+        },
+    },
+    OTHER: {
+        title: "Outro",
+        image: {
+            source: thoughtImageUrl,
+            alt: "Imagem de um balão de pensamento"
+        },
+    }
+}
+
 export function WidgetForm(){
 
     return (
