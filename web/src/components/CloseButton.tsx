21394b29cf569f4b17124a436513726336c02f8f
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-05 17:04:55 -0300

    estilizando o componente closebutton no header

---
+++ web/src/components/CloseButton.tsx
@@ -0,0 +1,5 @@
+export function CloseButton(){
+    return(
+        <span>x</span>
+    )
+}
--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,16 +1,18 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm(){
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2x1 mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem) md:w-auto]">
             
             <header>
-                <h2>Header</h2>
+                <h2>Deixe seu Feedback <CloseButton/></h2>
             </header>
 
             <p>Hello Word, dev!</p>
             
-            <footer>
-                <h2>Footer</h2>
+            <footer className="text-xs text-neutral-400">
+                Feito com 💜 <a href="https://google.com.br" className="underline underline-offset-2">Rocketseat</a>
             </footer>
             
         </div>
