358df8313002f0f44c447085091859af55547d23
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-09 18:05:52 -0300

    key button reactjs

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -41,7 +41,11 @@ export function WidgetForm(){
             <div className="flex py-8 gap-2 w-full">
                 { Object.entries(feedbackTypes).map(([key, value]) => {
                     return (
-                        <button className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none">
+                        <button 
+                            key={key}
+                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                            type="button"
+                        >
                             <img src={value.image.source} alt={value.image.alt} />
                             <span>{value.title}</span>
                         </button>
