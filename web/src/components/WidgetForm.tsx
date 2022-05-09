0c5145d323e903af71f89a01ded17110e118acbc
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-09 18:00:36 -0300

    texto em inglês

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -6,21 +6,21 @@ import thoughtImageUrl from "../assets/thought.svg";
 
 const feedbackTypes = {
     BUG: {
-        title: "Problema",
+        title: "Problem",
         image: {
             source: bugImageUrl,
             alt: "Imagem de um inseto"
         },
     },
     IDEA: {
-        title: "Ideia",
+        title: "Idea",
         image: {
             source: ideaImageUrl,
             alt: "Imagem de uma lâmpada"
         },
     },
     OTHER: {
-        title: "Outro",
+        title: "Other",
         image: {
             source: thoughtImageUrl,
             alt: "Imagem de um balão de pensamento"
@@ -34,19 +34,23 @@ export function WidgetForm(){
         <div className="bg-zinc-900 p-4 relative rounded-2x1 mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem) md:w-auto]">
             
             <header>
-                <span className="mx-6">Deixe seu Feedback </span>
+                <span className="mx-6">Leave yours Feedback</span>
                 <CloseButton/>
             </header>
 
             <div className="flex py-8 gap-2 w-full">
                 { Object.entries(feedbackTypes).map(([key, value]) => {
-                    console.log(key,value);
-                    return null;
+                    return (
+                        <button className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none">
+                            <img src={value.image.source} alt={value.image.alt} />
+                            <span>{value.title}</span>
+                        </button>
+                    );
                 }) }
             </div>
             
             <footer className="text-xs text-neutral-400">
-                Feito com 💜 <a href="https://google.com.br" className="underline underline-offset-2">Rocketseat</a>
+                Done with 💜 <a href="https://google.com.br" className="underline underline-offset-2">Rocketseat</a>
             </footer>
             
         </div>
