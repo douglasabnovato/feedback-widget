c0a9a1065d1b69ac4704d121c3a43919ec980923
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-10 13:32:14 -0300

    clique dos tipos de feedback

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -30,9 +30,11 @@ const feedbackTypes = {
     },
 }
 
+type FeedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm(){
 
-    const [feedbackTypes, setFeedbackTypes] = useState(null)
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2x1 mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem) md:w-auto]">
@@ -42,21 +44,25 @@ export function WidgetForm(){
                 <CloseButton/>
             </header>
 
-            <div className="flex py-8 gap-2 w-full">
-                { Object.entries(feedbackTypes).map(([key, value]) => {
-                    return (
-                        <button 
-                            key={key}
-                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            type="button"
-                            onClick={() => setFeedbackTypes(key)}
-                        >
-                            <img src={value.image.source} alt={value.image.alt} />
-                            <span>{value.title}</span>
-                        </button>
-                    );
-                }) }
-            </div>
+            {!feedbackType ? ( 
+                <div className="flex py-8 gap-2 w-full">
+                    { Object.entries(feedbackTypes).map(([key, value]) => {
+                        return (
+                            <button 
+                                key={key}
+                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                                type="button"
+                                onClick={() => setFeedbackType(key as FeedbackType)}
+                            >
+                                <img src={value.image.source} alt={value.image.alt} />
+                                <span>{value.title}</span>
+                            </button>
+                        );
+                    }) }
+                </div> 
+            ) : (
+                <p>Hello World.</p>
+            )}
             
             <footer className="text-xs text-neutral-400">
                 Done with 💜 <a href="https://google.com.br" className="underline underline-offset-2">Rocketseat</a>
