c8dd11ba09a0c9497bd34db326bbe6635e8832f2
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-10 18:59:37 -0300

    estilização do elemento textarea

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -30,8 +30,12 @@ export function FeedbackContentStep({
                 </span>
                 <CloseButton/>
             </header>
-            <div className="flex py-8 gap-2 w-full">                
-            </div>
+            <form className="my-4 w-full">
+                <textarea
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    placeholder="Conte com detalhes o que está acontecendo..."
+                ></textarea>
+            </form>
         </> 
     )
 }
