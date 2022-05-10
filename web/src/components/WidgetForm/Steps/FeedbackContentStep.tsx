d87f8ef1339f3e508c1f255136994b78388871f1
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-10 18:01:30 -0300

    botão voltar

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -4,16 +4,24 @@ import { CloseButton } from "../../CloseButton";
 
 interface FeedbackContentStepProps{
     feedbackType: FeedbackType;
+    onFeedbackRestartRequested: () => void;
 }
 
-export function FeedbackContentStep({feedbackType}:FeedbackContentStepProps){
+export function FeedbackContentStep({
+    feedbackType, 
+    onFeedbackRestartRequested
+}: FeedbackContentStepProps){
 
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     return (
         <>
             <header>
-                <button type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100">
+                <button 
+                    type="button" 
+                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+                    onClick={onFeedbackRestartRequested}
+                >
                     <ArrowLeft weight="bold" className="w-4 h-4"/>
                 </button>
                 <span className="text-xl leading-6 flex items-center gap-2">
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -34,13 +34,22 @@ export const feedbackTypes = {
 export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm(){
+
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+    function handleRestartFeedback(){
+        setFeedbackType(null);
+    }
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2x1 mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem) md:w-auto]">                        
             {!feedbackType ? ( 
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
             ) : (
-                <FeedbackContentStep feedbackType={feedbackType}/>
+                <FeedbackContentStep 
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartFeedback}
+                />
             )}            
             <footer className="text-xs text-neutral-400">
                 Done with 💜 <a href="https://google.com.br" className="underline underline-offset-2">Rocketseat</a>
