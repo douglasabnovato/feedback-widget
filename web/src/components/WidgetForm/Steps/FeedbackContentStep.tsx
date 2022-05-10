b0a022b335cf9c80268ad056367402539d7d7f68
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-10 19:26:03 -0300

    botão de enviar feedback

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -35,6 +35,12 @@ export function FeedbackContentStep({
                     className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..."
                 ></textarea>
+                <footer className="flex gap-2 mt-2">
+                    <button
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                    >Enviar Feedback</button>
+                </footer>
             </form>
         </> 
     )
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,8 +4,12 @@ module.exports = {
     extend: {
       colors:{
         brand:{
+          300: "#996DFF",
           500: "#8257e6",
         }
+      },
+      borderRadius:{
+        md: "4px",
       }
     },
   },
