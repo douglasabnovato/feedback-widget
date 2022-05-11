248d4612a20b986474de348981a1d9320b564634
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-11 16:43:40 -0300

    desabilitar botão de enviar quando estiver sem texto

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 import { ArrowLeft } from "phosphor-react";
+
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -15,9 +16,19 @@ export function FeedbackContentStep({
     onFeedbackRestartRequested
 }: FeedbackContentStepProps){
 
-    const [screenshot, setScreenshot] = useState<string | null>(null)
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+    const [comment, setComment] = useState("");
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
+    function handleSubmitFeedback(event: FormEvent){
+        event.preventDefault();
+        console.log({
+            screenshot,
+            comment,
+        })
+    }
+    
     return (
         <>
             <header>
@@ -34,10 +45,11 @@ export function FeedbackContentStep({
                 </span>
                 <CloseButton/>
             </header>
-            <form className="my-4 w-full">
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea
                     className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="tell me in detail what is going on..."
+                    onChange={event => setComment(event.target.value)}
                 ></textarea>
                 <footer className="flex gap-2 mt-2">
                     <ScreenshotButton
@@ -45,8 +57,9 @@ export function FeedbackContentStep({
                         onScreenshotTook={setScreenshot}
                     />
                     <button
+                        disabled={comment.length === 0}
                         type="submit"
-                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >Enviar Feedback</button>
                 </footer>
             </form>
