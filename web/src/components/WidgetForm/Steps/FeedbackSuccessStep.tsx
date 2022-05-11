52693adc821acced8a021ce23408743c4fad2e69
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-11 18:11:13 -0300

    botão enviar outro

--- web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,6 +1,10 @@
 import { CloseButton } from "../../CloseButton";
 
-export function FeedbackSuccessStep() {
+interface FeedbackSuccessStepProps{
+    onFeedbackRestartRequested: () => void
+}
+
+export function FeedbackSuccessStep({onFeedbackRestartRequested}:FeedbackSuccessStepProps) {
     return (
         <>
             <header>
@@ -13,6 +17,8 @@ export function FeedbackSuccessStep() {
                 </svg>
                 <span className="text-xl mt-2">Agradecemos o Feedback</span>
                 <button
+                    type="button"
+                    onClick={onFeedbackRestartRequested}
                     className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
                 >
                     Quero enviar outro
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -40,13 +40,14 @@ export function WidgetForm() {
     const [feedbackSent, setFeedbackSent] = useState(false);
 
     function handleRestartFeedback() {
+        setFeedbackSent(false)
         setFeedbackType(null);
     }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2x1 mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem) md:w-auto]">
             {feedbackSent ? (
-                <FeedbackSuccessStep />
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
             ) : (
                 <>
                     {!feedbackType ? (
