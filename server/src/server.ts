7ad7e3bdaa15f29bf14a8f1942a196928e88603e
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-12 11:27:43 -0300

    estruturar bd com o prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 
-const app = express()
+const app = express();
+
+app.get("/users", (request, response) => {
+    return response.send("Hello World, Dev.");
+})
 
 app.listen(3333, () => {
-    console.log("HTTP server running")
+    console.log("HTTP server running");
 })
