2cac40ef794cfe63fcc98ebcf9a658768fd42ed0
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-12 18:06:09 -0300

    criando a estrutura no bd do feedback via codigo com prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import { prisma } from "./prisma";
 
 const app = express();
 
-app.post("/users", (request, response) => {
-    return response.send("Hello World, Dev.");
-})
+app.use(express.json());
 
 app.post("/feedbacks", (request, response) => {
+    prisma.feedback.create({
+        data: {
+            type: request.body.type,
+            comment: request.body.comment,
+            screenshot: request.body.screenshot,
+        }
+    })
+
     return response.send("Hello World, Dev!!");
 })
 
