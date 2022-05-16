885ca0a552ce5b61ffd162f917dd80e3bd4154dd
Douglas A B Novato <douglasabnovato@gmail.com>
2022-05-16 13:48:46 -0300

    configuração para deploy do web

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,5 +1,6 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: 'http://localhost:3333'
 })
